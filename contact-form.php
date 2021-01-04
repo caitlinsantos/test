d5bb81b4d25cb7c06a1aea36b65bed0dbc95f585
caitlinsantos <57126096+caitlinsantos@users.noreply.github.com>
2021-01-04 01:15:00 -0500

    contact form

--- contact-form.php
+++ contact-form.php
@@ -1,25 +1,23 @@
 <?php
 
-$name = $_POST['name'];
-    $visitor_email = $_POST['email'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'santos.caitlin@gmail.com';
+    $email_from = 'YaziCole';
 
-    $email_subject = "New Form Submission";
+    $email_subject = "New Message from YaziCole";
 
-    $email_body = "User Name: $name.\n".
-        "User Email: $visitor_email.\n".
-        "User Message: $message.\n";
+    $email_body = "Name: $name.\n".
+                  "Email: $visitor_email.\n".
+                  "Message: $message.\n";
 
     $to = "green.does.everything@gmail.com";
-
     $headers = "From: $email_from \r\n";
-
-    $headers = "Reply-To: $visitor_email \r\n";
+    $headers .= "Reply-To: $email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
-    header("Location: index.html");
+    header("location: index.html");
 
 ?>
